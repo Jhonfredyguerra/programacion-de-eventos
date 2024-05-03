aa89b95a04a6747b053732ee1d6622b4eeae0afc
Jhonfredyguerra <125101731+Jhonfredyguerra@users.noreply.github.com>
2024-05-02 21:07:29 -0500

    Delete src directory

--- src/carrera.java
+++
@@ -1,9 +0,0 @@
-class Carrera {
-    Long codigo;
-    String nombre;
-    // Constructores, getters, setters, etc
-    public Carrera(Long codigo, String nombre) {
-        this.codigo = codigo;
-        this.nombre = nombre;
-    }
-}
